f00ff53f558ef9d5f5020b4fccbfdd4ce258e17a
rorita0x <0x@rorita.moe>
2026-05-16 20:01:03 +0200

    use user agent from session

--- src/index.js
+++ src/index.js
@@ -154,7 +154,7 @@ export default async function cf_fetch(url, options={}, method='get') {
         ja3: '771,4865-4866-4867-49195-49199-49196-49200-52393-52392-49171-49172-156-157-47-53,18-16-11-27-17613-35-23-0-45-65281-13-43-5-65037-10-51-41,4588-29-23-24,0',
         ja4r: 't13d1517h2_002f,0035,009c,009d,1301,1302,1303,c013,c014,c02b,c02c,c02f,c030,cca8,cca9_0005,000a,000b,000d,0012,0017,001b,0023,0029,002b,002d,0033,44cd,fe0d,ff01_0403,0804,0401,0503,0805,0501,0806,0601',
         http2Fingerprint: '1:65536;2:0;4:6291456;6:262144|15663105|0|m,a,s,p',
-        userAgent: "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/148.0.0.0 Safari/537.36", // session.headers["user-agent"],
+        userAgent: session.headers["user-agent"], // "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/148.0.0.0 Safari/537.36"
         headers: {
             ...options.headers,
             ...session.headers,
